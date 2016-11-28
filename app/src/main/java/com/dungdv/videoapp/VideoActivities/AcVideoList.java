488d31615190bad6ca818b9e1f8933764dfbe8ce
fukemy <fukemy@Yahoo.com>
2016-11-28 17:19:39 +0700

    initial commit

--- app/src/main/java/com/dungdv/videoapp/VideoActivities/AcVideoList.java
+++ app/src/main/java/com/dungdv/videoapp/VideoActivities/AcVideoList.java
@@ -3,6 +3,7 @@ package com.dungdv.videoapp.VideoActivities;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -14,6 +15,7 @@ import com.dungdv.videoapp.Adapters.VideoListAdapter;
 import com.dungdv.videoapp.Entities.EnVideoData;
 import com.dungdv.videoapp.R;
 import com.dungdv.videoapp.Utilities.GlobalParams;
+import com.github.pedrovgs.DraggableListener;
 import com.github.pedrovgs.DraggableView;
 
 import java.util.ArrayList;
@@ -42,28 +44,63 @@ public class AcVideoList extends Activity {
         draggableView = (DraggableView) findViewById(R.id.draggable_view);
         draggableView.setVisibility(View.GONE);
         draggableView.setClickToMaximizeEnabled(true);
-        draggableView.setClickToMinimizeEnabled(true);
+        draggableView.setClickToMinimizeEnabled(false);
 
+        draggableView.setDraggableListener(new DraggableListener() {
+            @Override
+            public void onMaximized() {
+                if(draggableView.getVisibility() == View.GONE)
+                    draggableView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onMinimized() {
+
+            }
+
+            @Override
+            public void onClosedToLeft() {
+
+            }
+
+            @Override
+            public void onClosedToRight() {
+
+            }
+        });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 EnVideoData videoData = (EnVideoData) parent.getAdapter().getItem(position);
+                Toast.makeText(AcVideoList.this, "click in pos: " + position, Toast.LENGTH_SHORT).show();
+
+                draggableView.setVisibility(View.VISIBLE);
                 draggableView.maximize();
             }
         });
+
     }
 
+    EnVideoData videoData;
     private void populateData(){
-
-        Toast.makeText(this, "populateData: " + videoList.size(), Toast.LENGTH_SHORT).show();
         videoList = new ArrayList<>();
-        EnVideoData videoData = new EnVideoData();
-        videoData.setVideoDescription("test");
-        videoData.setVideoName("test");
+        videoData = new EnVideoData();
+        videoData.setVideoDescription("test 1");
+        videoData.setVideoName("Nguyen Van Cu");
+        videoData.setVideoUrl("http://nvc42.ddns.net:81/viewer/main.html");
+        videoData.setVideoThumb(GlobalParams.THUMBS_VIDEO_SAMPLE);
+        videoList.add(videoData);
+
+        videoData = new EnVideoData();
+        videoData.setVideoDescription("test 2");
+        videoData.setVideoName("Ho Chi Minh");
         videoData.setVideoUrl("http://nvc42.ddns.net:81/viewer/main.html");
         videoData.setVideoThumb(GlobalParams.THUMBS_VIDEO_SAMPLE);
 
+
+        videoList.add(videoData);
+        Toast.makeText(this, "populateData: " + videoList.size(), Toast.LENGTH_SHORT).show();
         adapter = new VideoListAdapter(videoList, this);
         listView.setAdapter(adapter);
     }
